13debf4ff4d3d0aacd1ecde959d7df36b77796a6
raretimer <31965788+raretimer@users.noreply.github.com>
2018-01-18 11:47:51 -0600

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,24 @@
+#Latimer Ferrel
+
+# List
+b = [0,1,'mouse','d',3.14 ]
+
+
+print(b)
+
+#print an index of the list :
+
+print(b[2])
+
+
+
+
+# Map
+
+c = {0:'Yellow',1:'Red',3:'Blue' }   # Empty map
+
+c[0] = 'Green'
+
+print(c)
+print(c[1])
+
